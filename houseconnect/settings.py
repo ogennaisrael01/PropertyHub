1e5a8dde654b499d2dd4fbab05469e2a3c573330
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-08-06 14:49:01 +0100

    skeleton project

--- houseconnect/settings.py
+++ houseconnect/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'chats.apps.ChatsConfig',
+    'payments.apps.PaymentConfig',
+    'houses.apps.HousesConfig'
 ]
 
 MIDDLEWARE = [
@@ -72,10 +80,15 @@ WSGI_APPLICATION = 'houseconnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv("ENGINE"),
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("HOST"),
+        "PORT": os.getenv("PORT")
     }
 }
 
